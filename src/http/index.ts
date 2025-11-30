6deb9e3012d867b5ac9c3f72ce408c406289d95d
venerguevarrapv <119896894+venerguevarrapv@users.noreply.github.com>
2025-11-30 15:08:58 -0800

    Add environment variables

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,23 +1,29 @@
 import { createHttpClient } from "./client";
 import { localStorageTokenStorage } from "./tokenStorage";
 import { refreshTokenRequest } from "./refresh";
-import type { HttpClientOptions } from "./types";
+import type { AuthRequestConfig, HttpClientOptions } from "./types";
+import { pravitaAdminApiBaseUrl } from "../env.ts";
+import type { AxiosInstance, AxiosResponse } from "axios";
 
-const baseURL: string = "/api"; // TODO: change to your backend base URL or env var
+const baseURL: string = pravitaAdminApiBaseUrl;
 
 const options: HttpClientOptions = {
     baseURL,
     tokenStorage: localStorageTokenStorage,
     refreshToken: refreshTokenRequest,
-    onLogout: () => {
+    onLogout: (): void => {
         window.location.href = "/login";
     },
 };
 
 export const http = createHttpClient(options);
 
-export const axiosInstance = http.axios;
-export const requestWithCancel = http.requestWithCancel;
+export const axiosInstance: AxiosInstance = http.axios;
+export const requestWithCancel: <T = unknown>(config: AuthRequestConfig) => {
+    promise: Promise<AxiosResponse<T>>;
+    cancel: () => void;
+    signal: AbortSignal
+} = http.requestWithCancel;
 
 export * from "./types";
 export * from "./tokenStorage";
