1b6f56a34eeed9b3602095cd9a351b5af1b90526
venerguevarrapv <119896894+venerguevarrapv@users.noreply.github.com>
2025-12-07 21:26:12 -0800

    Fetch current user info

--- src/api/hubApiClient.ts
+++ src/api/hubApiClient.ts
@@ -1,11 +1,6 @@
 import { Configuration } from "./hub-api/__openapi-generated/configuration";
 import {
-    AuthenticationApi,
-    TenantsApi,
-    RolesApi,
-    SystemModulesApi,
-    RegistrationApi,
-    UserActivationCodeApi,
+    AuthenticationApi, TenantsApi, RolesApi, SystemModulesApi, RegistrationApi, UserActivationCodeApi, CurrentUserApi,
 } from "./hub-api/__openapi-generated/api";
 
 import { axiosInstance } from "../http";
@@ -15,30 +10,16 @@ const configuration: Configuration = new Configuration({
     basePath: pravitaAdminApiBaseUrl,
 });
 
-export const authenticationApi: AuthenticationApi = new AuthenticationApi(
-    configuration,
-    configuration.basePath,
-    axiosInstance,
-);
+export const authenticationApi: AuthenticationApi = new AuthenticationApi(configuration, configuration.basePath, axiosInstance);
 
-export const registrationApi: RegistrationApi = new RegistrationApi(
-    configuration,
-    configuration.basePath,
-    axiosInstance,
-);
+export const registrationApi: RegistrationApi = new RegistrationApi(configuration, configuration.basePath, axiosInstance);
 
-export const userActivationCodeApi: UserActivationCodeApi = new UserActivationCodeApi(
-    configuration,
-    configuration.basePath,
-    axiosInstance,
-);
+export const userActivationCodeApi: UserActivationCodeApi = new UserActivationCodeApi(configuration, configuration.basePath, axiosInstance);
+
+export const currentUserApi: CurrentUserApi = new CurrentUserApi(configuration, configuration.basePath, axiosInstance);
 
 export const tenantsApi: TenantsApi = new TenantsApi(configuration, configuration.basePath, axiosInstance);
 
 export const rolesApi: RolesApi = new RolesApi(configuration, configuration.basePath, axiosInstance);
 
-export const systemModulesApi: SystemModulesApi = new SystemModulesApi(
-    configuration,
-    configuration.basePath,
-    axiosInstance,
-);
+export const systemModulesApi: SystemModulesApi = new SystemModulesApi(configuration, configuration.basePath, axiosInstance);
